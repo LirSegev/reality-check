8663b7f01f067a67ded0b41c99d8a8dc136d7855
Lir Segev <lir.segev@gmail.com>
2019-07-24 00:20:41 +0200

    Add map to map tab

--- client/src/routes/game/operation/map/Map.tab.view.tsx
+++ client/src/routes/game/operation/map/Map.tab.view.tsx
@@ -1,11 +1,42 @@
 import * as React from 'react';
 import { Page } from 'react-onsenui';
 
+import ReactMapboxFactory, { MapContext } from 'react-mapbox-gl';
+import { GeolocateControl } from 'mapbox-gl';
+import mapboxConfig from '../../../../config/Mapbox';
+
+const Map = ReactMapboxFactory({
+	accessToken: mapboxConfig.accessToken,
+});
+
 const MapTabView: React.FC = () => (
 	<Page>
-		<section>
-			<p>Map Tab</p>
-		</section>
+		<Map
+			// eslint-disable-next-line
+			style={mapboxConfig.styleURL}
+			center={[14.42, 50.08]}
+			zoom={[12]}
+			containerStyle={{
+				height: '100%',
+				width: '100%',
+			}}
+		>
+			<MapContext.Consumer>
+				{/* Locate the user */
+				map => {
+					if (map)
+						map.addControl(
+							new GeolocateControl({
+								positionOptions: {
+									enableHighAccuracy: true,
+								},
+								trackUserLocation: true,
+							})
+						);
+					return <React.Fragment />;
+				}}
+			</MapContext.Consumer>
+		</Map>
 	</Page>
 );
 
