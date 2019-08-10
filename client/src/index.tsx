0a9b5af618771e05fef362db410c1167ebe518a1
Lir Segev <lir.segev@gmail.com>
2019-08-10 13:19:04 +0200

    Add eruda
    Console viewer on mobile

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -14,6 +14,17 @@ import 'firebase/auth';
 import firebaseConfig from './config/firebase';
 firebase.initializeApp(firebaseConfig);
 
+(function() {
+	var src = '//cdn.jsdelivr.net/npm/eruda';
+	if (
+		!/eruda=true/.test(window.location.href) &&
+		localStorage.getItem('active-eruda') !== 'true'
+	)
+		return;
+	document.write(`<script src="${src}"></script>`);
+	document.write('<script>eruda.init();</script>');
+})();
+
 ons.ready(() =>
 	ReactDOM.render(<AppContainer />, document.getElementById('root'))
 );
