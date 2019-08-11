b8e4a7ddb4ee6f48980589bc211fb0237fb2a37e
Lir Segev <lir.segev@gmail.com>
2019-08-11 13:55:51 +0200

    Add config/ dir to git

---
+++ client/src/config/firebase.ts
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+	apiKey: 'AIzaSyBcPUPtr_37iqBlCUHpJhZy5cP8FBKqNY4',
+	authDomain: 'reality-check-d6fe7.firebaseapp.com',
+	databaseURL: 'https://reality-check-d6fe7.firebaseio.com',
+	projectId: 'reality-check-d6fe7',
+	storageBucket: 'reality-check-d6fe7.appspot.com',
+	messagingSenderId: '243239923760',
+	appId: '1:243239923760:web:f6bc0b9da0893fa7',
+};
+export default firebaseConfig;
