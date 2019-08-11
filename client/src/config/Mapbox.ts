63f0819a9e975ed2529303c8a8ef41f84566a4bb
Lir Segev <lir.segev@gmail.com>
2019-08-11 13:55:51 +0200

    Add config/ dir to git

---
+++ client/src/config/Mapbox.ts
@@ -0,0 +1,7 @@
+const mapboxConfig = {
+	accessToken:
+		'pk.eyJ1IjoibGlyLXNlZ2V2IiwiYSI6ImNqeWJsbXdkdTA5b20zZG56MzQ2ZDFpYXMifQ.NVYbXPa3GQx6WD-qXQb2mw',
+	styleURL: 'mapbox://styles/lir-segev/cjyfoyxd50ear1cp6rxq6ftyo/draft',
+};
+
+export default mapboxConfig;
