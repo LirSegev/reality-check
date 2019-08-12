75b6ab2883a00344d51c483f0fea62a92c520468
Lir Segev <lir.segev@gmail.com>
2019-08-13 00:01:59 +0200

    Refractor
    Move manageDeviceGroup() to util.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,7 @@
 import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
-import fetch from 'node-fetch';
-import cloudMessagingConfig from './config/messaging';
 import { HttpsError } from 'firebase-functions/lib/providers/https';
+import { manageDeviceGroup } from './util';
 
 admin.initializeApp(functions.config().firebase);
 const db = admin.firestore();
@@ -100,62 +99,3 @@ export const removeDeviceFromDeviceGroup = functions.https.onCall(
 			});
 	}
 );
-
-/**
- * Add, or remove registration tokens from Firebase Cloud Messaging device group
- * @param operation
- * @param ids Array of registration tokens to be used
- * @param groupName Unique notification_key_name
- * @param notificationKey notification_key of the group.
- */
-function manageDeviceGroup(
-	operation: 'add' | 'remove',
-	ids: string[],
-	groupName: string,
-	notificationKey?: string
-): Promise<string>;
-/**
- * Create new Firebase Cloud Messaging device group
- * @param operation
- * @param ids Array of registration tokens to be used
- * @param groupName Unique notification_key_name
- */
-
-function manageDeviceGroup(
-	operation: 'create',
-	ids: string[],
-	groupName: string
-): Promise<string>;
-function manageDeviceGroup(
-	operation: 'create' | 'add' | 'remove',
-	ids: string[],
-	groupName: string,
-	notificationKey?: string
-) {
-	const url = 'https://fcm.googleapis.com/fcm/notification';
-	const body: any = {
-		operation,
-		notification_key_name: groupName,
-		registration_ids: ids,
-	};
-	if (notificationKey) body.notification_key = notificationKey;
-
-	return fetch(url, {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-			Authorization: `key=${cloudMessagingConfig.serverKey}`,
-			project_id: cloudMessagingConfig.senderId,
-		},
-		body: JSON.stringify(body),
-	})
-		.then(res => res.json())
-		.then(res => {
-			if (res.notification_key) return res.notification_key as string;
-			else if (res.error) throw new Error(res.error);
-			else {
-				console.error(res);
-				throw new Error();
-			}
-		});
-}
