ab629b2a5952b7689354698a2e1f66c105d33dc1
Lir Segev <lir.segev@gmail.com>
2019-09-23 13:39:27 +0300

    Refractor

--- client/src/util/firebase.ts
+++ client/src/util/firebase.ts
@@ -1,27 +1,34 @@
 import * as firebase from 'firebase/app';
 import { getCurrentPlayer, updateCurrentPlayer } from './db';
 
+const removePlayerFromDeviceGroup = (gameId: string) => (
+	player: Player | undefined
+) => {
+	if (player && player.messagingToken) {
+		const removeDeviceFromDeviceGroup = firebase
+			.functions()
+			.httpsCallable('removeDeviceFromDeviceGroup');
+
+		removeDeviceFromDeviceGroup({
+			token: player.messagingToken,
+			gameId,
+		});
+	} else {
+		console.error(new Error("Player doesn't have messagingToken"));
+	}
+};
+
 export function signOut(isAdmin?: boolean) {
+	/**
+	 * Array of promises to complete before signing user out.
+	 */
 	const doBeforeSignOut: Promise<any>[] = [];
 
 	if (!isAdmin) {
 		const gameId = localStorage.getItem('gameId');
 		if (gameId) {
 			doBeforeSignOut.push(
-				getCurrentPlayer().then(player => {
-					if (player && player.messagingToken) {
-						const removeDeviceFromDeviceGroup = firebase
-							.functions()
-							.httpsCallable('removeDeviceFromDeviceGroup');
-
-						removeDeviceFromDeviceGroup({
-							token: player.messagingToken,
-							gameId,
-						});
-					} else {
-						console.error(new Error("Player doesn't have messagingToken"));
-					}
-				})
+				getCurrentPlayer().then(removePlayerFromDeviceGroup(gameId))
 			);
 			doBeforeSignOut.push(
 				updateCurrentPlayer({
