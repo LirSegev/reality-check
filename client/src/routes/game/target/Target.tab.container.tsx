a969c84b419347d68b621339750de7f398717eb9
Lir Segev <lir.segev@gmail.com>
2019-10-10 01:14:07 +0200

    Create mock target tab

---
+++ client/src/routes/game/target/Target.tab.container.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import TargetTabView from './Target.tab.view';
+
+class TargetTabContainer extends React.Component<{}> {
+	render() {
+		return (
+			<TargetTabView
+				imgSrc={[
+					'https://bloximages.chicago2.vip.townnews.com/apg-wi.com/content/tncms/assets/v3/editorial/b/57/b57bcd7c-15c0-11e9-aba0-079136c9cda6/5c38c8e7824ab.image.jpg?resize=400%2C362',
+				]}
+			/>
+		);
+	}
+}
+
+export default TargetTabContainer;
--- client/src/routes/game/target/Target.tab.view.tsx
+++ client/src/routes/game/target/Target.tab.view.tsx
@@ -1,12 +1,37 @@
 import * as React from 'react';
 import { Page } from 'react-onsenui';
+import styles from './Target.module.css';
 
-const TargetTabView: React.FC = () => (
-	<Page>
-		<section>
-			<p>Target Tab</p>
-		</section>
-	</Page>
-);
+interface Props {
+	imgSrc: string[];
+}
+
+const TargetTabView: React.FC<Props> = props => {
+	const imgEls = props.imgSrc.map(src => <img src={src} alt="" />);
+
+	return (
+		<Page>
+			<div id={styles.wrapper}>
+				<div id={styles.suspects}>
+					<div id={styles.images}>{imgEls}</div>
+				</div>
+				<section id={styles.clues}>
+					<table>
+						<tbody>
+							<tr>
+								<td>Accessories</td>
+								<td>Glasses</td>
+							</tr>
+							<tr>
+								<td>Facial Hair</td>
+								<td>None</td>
+							</tr>
+						</tbody>
+					</table>
+				</section>
+			</div>
+		</Page>
+	);
+};
 
 export default TargetTabView;
