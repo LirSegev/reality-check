052239ebb90377e15caca07262f147c358af5aa7
Lir Segev <lir.segev@gmail.com>
2019-10-11 17:39:06 +0200

    Change map style

--- client/src/config/Mapbox.ts
+++ client/src/config/Mapbox.ts
@@ -1,7 +1,7 @@
 const mapboxConfig = {
 	accessToken:
 		'pk.eyJ1IjoibGlyLXNlZ2V2IiwiYSI6ImNqeWJsbXdkdTA5b20zZG56MzQ2ZDFpYXMifQ.NVYbXPa3GQx6WD-qXQb2mw',
-	styleURL: 'mapbox://styles/lir-segev/ck02ca9xz06hw1cpozamnwk6i/draft',
+	styleURL: 'mapbox://styles/lir-segev/ck1m85cl70cca1cmlciecihoo/draft',
 };
 
 export default mapboxConfig;
