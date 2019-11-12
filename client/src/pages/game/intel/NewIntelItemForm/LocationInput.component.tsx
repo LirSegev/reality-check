1f0001a93739565efc83b29af08ef19d51e22cab
Lir Segev <lir.segev@gmail.com>
2019-11-12 23:06:15 +0100

    Make icons bigger

--- client/src/pages/game/intel/NewIntelItemForm/LocationInput.component.tsx
+++ client/src/pages/game/intel/NewIntelItemForm/LocationInput.component.tsx
@@ -9,39 +9,35 @@ interface Props {
 	onMapLocation: () => void;
 }
 
-const MoreInputLocationInput: React.FC<Props> = props => (
-	<div style={{ display: 'flex' }}>
-		<Button
-			modifier="quiet"
-			style={{
-				display: 'inline-block',
-				marginRight: '5px',
-				color: props.location ? '' : '#000',
-				flex: '0 0 auto',
-			}}
-			onClick={props.onMyLocation}
-		>
-			<Icon style={{ height: '32px' }} icon="md-gps-dot" />{' '}
-		</Button>
-		<Button
-			modifier="quiet"
-			style={{
-				display: 'inline-block',
-				marginRight: '5px',
-				color: props.location ? '' : '#000',
-				flex: '0 0 auto',
-			}}
-			onClick={props.onMapLocation}
-		>
-			<Icon style={{ height: '32px' }} icon="md-map" />{' '}
-		</Button>
-		<Input
-			onChange={props.onChange}
-			value={props.value}
-			modifier="underbar"
-			type="text"
-		/>
-	</div>
-);
+const MoreInputLocationInput: React.FC<Props> = props => {
+	const buttonStyle = {
+		display: 'inline-block',
+		marginRight: '5px',
+		color: props.location ? '' : '#000',
+		flex: '0 0 auto',
+		padding: '0.25em',
+	};
+
+	return (
+		<div style={{ display: 'flex' }}>
+			<Button modifier="quiet" style={buttonStyle} onClick={props.onMyLocation}>
+				<Icon icon="md-gps-dot" size={20} />{' '}
+			</Button>
+			<Button
+				modifier="quiet"
+				style={buttonStyle}
+				onClick={props.onMapLocation}
+			>
+				<Icon icon="md-map" size={20} />{' '}
+			</Button>
+			<Input
+				onChange={props.onChange}
+				value={props.value}
+				modifier="underbar"
+				type="text"
+			/>
+		</div>
+	);
+};
 
 export default MoreInputLocationInput;
