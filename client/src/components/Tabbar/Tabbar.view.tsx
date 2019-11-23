907429f690a131eefc26d028fbe9feea9ef8b76d
Lir Segev <lir.segev@gmail.com>
2019-11-23 03:06:55 +0100

    Get tab and index from props

--- client/src/components/Tabbar/Tabbar.view.tsx
+++ client/src/components/Tabbar/Tabbar.view.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import styles from './Tabbar.module.css';
 
-const TabbarView: React.FC = () => (
-	<div className={styles.bar}>
-		<button className={`${styles.button} ${styles.active}`}>Test</button>
-		<button className={styles.button}>Test2</button>
-		<button className={styles.button}>Test3</button>
-		<button className={styles.button}>Test4</button>
-		<button className={styles.button}>Test5</button>
-		<button className={styles.button}>Test6</button>
-		<button className={styles.button}>Test7</button>
-	</div>
-);
+interface Props {
+	tabs: {
+		tabTitle: string;
+		content: JSX.Element;
+	}[];
+	index: number;
+	onChange?: (e: { index: number }) => void;
+}
+
+const TabbarView: React.FC<Props> = props => {
+	const tabs = props.tabs.map((tab, index) => {
+		const classes = [styles.button];
+		if (index === props.index) classes.push(styles.active);
+
+		return <button className={classes.join(' ')}>{tab.tabTitle}</button>;
+	});
+
+	return <div className={styles.bar}>{tabs}</div>;
+};
 
 export default TabbarView;
