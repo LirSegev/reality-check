4b64dd89e27fbf320c2dd82ac94c6d06b5150d61
Lir Segev <lir.segev@gmail.com>
2019-11-24 03:37:55 +0100

    Switch from redux-starter-kit to @reduxjs/toolkit

--- client/src/reducers/map.reducer.ts
+++ client/src/reducers/map.reducer.ts
@@ -1,4 +1,4 @@
-import { createSlice, PayloadAction } from 'redux-starter-kit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import initialState from './initialState';
 import { moveToLocationOnMap, changeTab } from './main.reducer';
 import { moveToLocationOnMapPayload, changeTabPayload } from './main.reducer.d';
