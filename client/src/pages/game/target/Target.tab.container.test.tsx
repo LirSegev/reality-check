822a6457f83906ebc98bc70d390f7abf44d56263
Lir Segev <lir.segev@gmail.com>
2019-11-28 17:09:46 +0100

    Fix Target.tab.container.test

--- client/src/pages/game/target/Target.tab.container.test.tsx
+++ client/src/pages/game/target/Target.tab.container.test.tsx
@@ -1,76 +1,147 @@
-import { render, cleanup } from '@testing-library/react';
+import { cleanup, render } from '@testing-library/react';
 import * as React from 'react';
-import { Props } from './Target.tab.view';
+import { ConnectedComponent, Provider } from 'react-redux';
+import { DeepPartial } from 'redux';
+import configureStore from 'redux-mock-store';
+
+import initialState, { ReduxState } from '../../../reducers/initialState';
+import TargetTabContainerImport from './Target.tab.container';
+import { Props as TargetTabViewProps } from './Target.tab.view';
+
+const mockStore = configureStore<ReduxState>();
+
+function withStore<P>(
+	Element: ConnectedComponent<React.ComponentType<any>, P>,
+	state: DeepPartial<ReduxState> & {
+		main: Pick<ReduxState['main'], 'tabIndex'>;
+	}
+) {
+	const baseState: ReduxState = {
+		...initialState,
+		main: {
+			...initialState.main,
+			gameId: 'someGameId',
+			isLoading: false,
+			tabIndex: 0,
+		},
+	};
+
+	const store = mockStore({
+		...baseState,
+		...state,
+	} as ReduxState);
+
+	return (props: JSX.LibraryManagedAttributes<typeof Element, P>) => (
+		<Provider store={store}>
+			<Element {...props} />
+		</Provider>
+	);
+}
 
 afterEach(cleanup);
-afterEach(jest.resetModules);
+// afterEach(jest.resetModules);
 
 it('renders', () => {
-	jest.mock('./Target.tab.view.tsx', () => () => <div />);
-	const { default: TargetTab } = require('./Target.tab.container');
-	const { asFragment } = render(<TargetTab />);
+	jest.isolateModules(() => {
+		jest.doMock('./Target.tab.view.tsx', () => () => <div />);
+		const { default: TargetTab } = require('./Target.tab.container');
+		const TargetTabWithStore = withStore(
+			TargetTab as typeof TargetTabContainerImport,
+			{
+				main: {
+					tabIndex: 0,
+				},
+			}
+		);
 
-	expect(asFragment()).toMatchSnapshot();
+		const { asFragment } = render(
+			<TargetTabWithStore tabIndex={0} incrementUnreadNum={() => true} />
+		);
+
+		expect(asFragment()).toMatchSnapshot();
+	});
 });
 
 it('updateSuspectList() works', () => {
-	jest.mock('./Target.tab.view.tsx', () =>
-		jest
-			.fn()
-			.mockImplementationOnce((props: Props) => {
-				props.updateSuspectList([1, 2]);
+	jest.isolateModules(() => {
+		jest.doMock('./Target.tab.view.tsx', () =>
+			jest
+				.fn()
+				.mockImplementationOnce((props: TargetTabViewProps) => {
+					props.updateSuspectList([1, 2]);
 
-				return <div />;
-			})
-			.mockImplementation(() => <div />)
-	);
-	const { default: TargetTab } = require('./Target.tab.container');
-	render(<TargetTab />);
+					return <div />;
+				})
+				.mockImplementation(() => <div />)
+		);
+		const { default: TargetTab } = require('./Target.tab.container');
+		const TargetTabWithStore = withStore(
+			TargetTab as typeof TargetTabContainerImport,
+			{
+				main: {
+					tabIndex: 0,
+				},
+			}
+		);
 
-	const TargetTabView = require('./Target.tab.view');
-	expect(TargetTabView).toHaveBeenNthCalledWith(
-		1,
-		expect.objectContaining({
-			suspectList: [],
-		}),
-		{}
-	);
-	expect(TargetTabView).toHaveBeenNthCalledWith(
-		2,
-		expect.objectContaining({
-			suspectList: [1, 2],
-		}),
-		{}
-	);
+		render(<TargetTabWithStore tabIndex={0} incrementUnreadNum={() => true} />);
+
+		const TargetTabView = require('./Target.tab.view');
+		expect(TargetTabView).toHaveBeenNthCalledWith(
+			1,
+			expect.objectContaining({
+				suspectList: [],
+			}),
+			{}
+		);
+		expect(TargetTabView).toHaveBeenNthCalledWith(
+			2,
+			expect.objectContaining({
+				suspectList: [1, 2],
+			}),
+			{}
+		);
+	});
 });
 
 it('selectSuspect() works', () => {
-	jest.mock('./Target.tab.view.tsx', () =>
-		jest
-			.fn()
-			.mockImplementationOnce((props: Props) => {
-				props.selectSuspect(1);
+	jest.isolateModules(() => {
+		jest.doMock('./Target.tab.view.tsx', () =>
+			jest
+				.fn()
+				.mockImplementationOnce((props: TargetTabViewProps) => {
+					props.selectSuspect(1);
 
-				return <div />;
-			})
-			.mockImplementation(() => <div />)
-	);
-	const { default: TargetTab } = require('./Target.tab.container');
-	render(<TargetTab />);
+					return <div />;
+				})
+				.mockImplementation(() => <div />)
+		);
+		const { default: TargetTab } = require('./Target.tab.container');
+		const TargetTabWithStore = withStore(
+			TargetTab as typeof TargetTabContainerImport,
+			{
+				main: {
+					tabIndex: 0,
+				},
+			}
+		);
 
-	const TargetTabView = require('./Target.tab.view');
-	expect(TargetTabView).toHaveBeenNthCalledWith(
-		1,
-		expect.objectContaining({
-			selectedSuspect: null,
-		}),
-		{}
-	);
-	expect(TargetTabView).toHaveBeenNthCalledWith(
-		2,
-		expect.objectContaining({
-			selectedSuspect: 1,
-		}),
-		{}
-	);
+		render(<TargetTabWithStore incrementUnreadNum={() => true} tabIndex={0} />);
+
+		const TargetTabView = require('./Target.tab.view');
+		expect(TargetTabView).toHaveBeenNthCalledWith(
+			1,
+			expect.objectContaining({
+				selectedSuspect: undefined,
+			}),
+			{}
+		);
+		expect(TargetTabView).toHaveBeenNthCalledWith(
+			2,
+			expect.objectContaining({
+				selectedSuspect: 1,
+			}),
+			{}
+		);
+	});
 });
