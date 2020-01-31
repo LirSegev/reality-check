44dd2beda7e2edcdd3b5f9dc535543453539d86b
Lir Segev <lir.segev@gmail.com>
2020-01-31 15:30:28 +0100

    Add create game button

--- client/src/pages/chooseGame/ChooseGame.page.container.tsx
+++ client/src/pages/chooseGame/ChooseGame.page.container.tsx
@@ -1,15 +1,21 @@
+import ons from 'onsenui';
 import * as React from 'react';
 import { connect } from 'react-redux';
 
+import { db } from '../..';
 import { ReduxState } from '../../reducers/initialState';
-import { changeGame, stopLoading } from '../../reducers/main.reducer';
+import {
+	changeGame,
+	stopLoading,
+	addNotification,
+} from '../../reducers/main.reducer';
 import ChooseGamePageView from './ChooseGame.page.view';
 import renderGameItem from './renderGameItem';
-import { db } from '../../index';
 
 interface Props {
 	stopLoading: ConnectedAction<typeof stopLoading>;
 	changeGame: ConnectedAction<typeof changeGame>;
+	addNotification: ConnectedAction<typeof addNotification>;
 }
 interface State {
 	gameList: string[];
@@ -27,21 +33,54 @@ class ChooseGamePageContainer extends React.Component<Props, State> {
 	_renderGameItem = renderGameItem(this.props.changeGame);
 
 	componentWillMount() {
+		this._getGameList();
+	}
+
+	_getGameList() {
 		db.collection('games')
 			.get()
 			.then(games => {
+				let gameList: string[] = [];
 				games.forEach(game => {
-					this.setState(prevState => ({
-						gameList: [...prevState.gameList, game.id],
-					}));
+					gameList.push(game.id);
+				});
+				this.setState({
+					gameList,
 				});
 				this.props.stopLoading();
 			})
 			.catch(err => console.error(new Error('Error getting game list:'), err));
 	}
 
+	_createGame = () => {
+		ons.notification
+			.prompt({
+				message: 'Enter code for game',
+				// @ts-ignore
+				placeholder: 'Game code',
+				title: 'Create game',
+			})
+			.then((name: string) => {
+				db.collection('games')
+					.doc(encodeURIComponent(name))
+					.set({ phase: 1 })
+					.then(() => {
+						this._getGameList();
+					});
+			})
+			.catch(() => {
+				this.props.addNotification({
+					notification: {
+						type: 'error',
+						header: 'There was an error creating a new game',
+					},
+				});
+			});
+	};
+
 	render = () => (
 		<ChooseGamePageView
+			createGame={this._createGame}
 			renderGameItem={this._renderGameItem}
 			gameList={this.state.gameList}
 		/>
@@ -54,5 +93,6 @@ const mapState = (state: ReduxState) => ({
 const mapDispatch = {
 	changeGame,
 	stopLoading,
+	addNotification,
 };
 export default connect(mapState, mapDispatch)(ChooseGamePageContainer);
